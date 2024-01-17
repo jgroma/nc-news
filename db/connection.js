6326bab2c60acbb3380a9317d53471fabe1e1c39
jgroma <jgromadzinska9@gmail.com>
2024-01-17 12:17:36 +0000

    Fix hosting set up

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,17 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
+//console.log(ENV);
 const config = {};
-
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+  //console.log(process.env.DATABASE_URL);
+}
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
